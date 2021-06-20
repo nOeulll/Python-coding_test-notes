50973b2c33e08be7a97c02e014e3eeee62c551b6
ru__ <bangawork7@gmail.com>
2021-06-21 01:00:05 +0900

    K번째수 - 다른 문제풀이 추가
    별거 아님

--- Sort/K번째수.py
+++ Sort/K번째수.py
@@ -4,3 +4,11 @@ def solution(array, commands):
         sort_array = sorted(array[commands[i][0]-1: commands[i][1]])
         answer.append(sort_array[commands[i][2]-1])
     return answer
+
+
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        sort_array = sorted(array[command[0]-1: command[1]])
+        answer.append(sort_array[command[2]-1])
+    return answer
