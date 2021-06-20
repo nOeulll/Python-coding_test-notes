7d551d573498cd158210a8036305653818f3ccb6
ru__ <bangawork7@gmail.com>
2021-06-20 14:53:17 +0900

    해시 - 완주하지 못한 선수
    연어의 도움

---
+++ Hash/완주하지 못한 선수.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    for i in range(len(participant)):
+        if participant[i] != completion[i] or i == len(participant)-1:
+            answer = participant[i]
+            break
+    return answer
